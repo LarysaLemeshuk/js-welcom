ad368783d170abaec68726c0e014edec2853fa55
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2024-12-10 18:04:21 +0100

    concatenation

--- index.js
+++ index.js
@@ -4,7 +4,41 @@ let box2;//undefined
 
 box2 = 15 // 15
 
-const secretWord = 'superSicret!Kyivstar!Cybersecuruti';
-console.log(secretWord);
+//const secretWord = 'superSicret!Kyivstar!Cybersecuruti';
+//console.log(secretWord);
 
-//secretWord = 'WORRRD' <<<<<--------перевизначити не можливо
+secretWord = 'WORRRD' //<<<<<--------перевизначити не можливо
+
+//console.log(box2);//не виконається через помилку на рядку 10
+//.... 
+
+let result1 = 1 + 2 ; // 3
+//console.log(result1);
+let result2 = 3 - 2; // 1 
+//console.log(result2);
+let result3 = 2 * 2 ;//4
+//console.log(result3);
+let result4 = 4 / 2; // 2
+//console.log(result4);
+
+let result5 = 0.1 + 0.2; // 0.3
+//console.log(result5);
+
+let notAllowed = 10 / 0;// infinity
+//console.log(notAllowed);// нескінченність
+
+let brrrr = 10 / 'HELLO WORLD';
+//console.log(brrrr); //NaN  
+// NaN- not a Number --->>> операція неприпустима з точки зору математики
+
+//"+"
+// складає числа
+// зклеює строки (конкатенація)
+let newLine = 'hello' + 'WORLD';
+//console.log(newLine);
+
+newLine = 5 + 'HELLO';
+//console.log(newLine);
+
+newLine = '1' + 1;
+console.log(newLine);
