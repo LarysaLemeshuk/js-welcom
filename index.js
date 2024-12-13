c7c03b5d7baf7636c3df1e16b715faebb1450964
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2024-12-13 17:03:59 +0100

    Practice task resolv

--- index.js
+++ index.js
@@ -1,43 +1,6 @@
-// Логічні оператори
+// у нааас є довжина та ширина прямокутника. S - ?
 
-// Працювати ці оператори будуть з логічним типом даних (Boolean)
-
-const a = true;
-
-// ! - заперечення
-!a; // false
-//!! - подвійне заперечення
-!!a;
-
-//console.log (Boolean(1));//явне приведення до типу Boolean
-
-//порівняння чисел
-
-const result1 = 4 > 5; // false
-const result2 = 7 < 9; // true
-
-//порівняння рядків
-const result3 = "a" > "A"; // 61 > 41 ---> true
-const result4 = "b" < "u"; // 62 < 75 ---> true
-
-const result5 = "test" > "word"; // 74 > 77 ---> false
-const result6 = "test" > "tord"; // 65 > 6F ---> false
-
-//перевірка рівності чисел
-
-// == - приводить операнди до одного типу даних і потім порівнює. не рекомндується
-
-const result7 = 5 == 6; // false
-const result8 = 6 == 6; // true
-const result9 = "9" == 9; // true
-
-// "===" - порівнює оператори з за значенням і за типом данихю рекомендується
-
-const result10 = "9" === 9; // false
-const result11 = 9 === 9; // true
-
-// більше - або- дорівнює або менше - або - дорівнює
-const result12 = 3 >= 3; // true
-const result13 = 6 <= 7; // true
-
-console.log(result7);
+const length = 5;
+const width = 8;
+const area = length + width;
+console.log("Площа прямокутника: " + area);
