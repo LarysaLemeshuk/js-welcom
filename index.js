3bdd212aaa11b534b58dafd8cf39c9f02918b22b
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2024-12-19 09:31:00 +0100

    Continue Functions

--- index.js
+++ index.js
@@ -15,11 +15,32 @@ function імʼя_функції(аргумент1, аргумент2) {
 
 // Задача- написати ффункцію, яка буде приймати імʼя користувача- строка і буде вітатись з цим користуачем
 
-function greetingUser(userName) {
+/*function greetingUser(userName) {
   console.log("Hi," + userName);
-}
+}*/
 // використання функції
-greetingUser("Alex"); // Alex - аргумент
+/*greetingUser("Alex"); // Alex - аргумент
 greetingUser("John");
 greetingUser("Sam");
-greetingUser("Solar");
+greetingUser("Solar");*/
+
+// задача- створіть функію, яка приймає число, і додає до нього 5 і виводить результат у консоль
+
+function addFiveToNumber(number) {
+  let result = number + 5;
+  console.log(result);
+  return result;
+}
+
+const addFiveToNumberResult = addFiveToNumber(5);
+
+// Написати функію яка складає 2 числа і повертає результат
+
+function addTwoNumbers(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+const addTwoNumbersResult = addTwoNumbers(2, 5);
+
+console.log(addFiveToNumber + addFiveToNumberResult); 
