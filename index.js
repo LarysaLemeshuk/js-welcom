a29e650ae580afed4a0e2ced5aadaf61fb16bb8a
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2024-12-21 16:56:19 +0100

    Ternar operator

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
-//const num1 = Number(prompt("Введіть перше число"));
-//const num2 = Number(prompt("Введіть друге число"));
+// Унарні, бінарні, тернарні
 
-// Задача. Написати функцію, яка приймає 2 числа і додає їх
+// Тернарний оператор - потребує 3-х операндів
 
-function addTwoNumbers(arg1, arg2) {
-  console.log(num1);
-  return arg1 + arg2;
-}
+// Тернарний оператор дозволяяє вибрати 1 з 2 операндів на основі заданої умови.
 
-let result = addTwoNumbers(num1, num2);
-console.log(result);
+// let result =  умова ? операнд1 : операнд2;
 
-//область видимості  - визначення доступності і видимості змінних та функцій у певних частинах коду
+// операнд1 - значення для змінноії result, якщо умова буде true
+// операнд2 - значення для змінноії result, якщо умова буде false
 
-/* 
+const age = 9;
 
- у JS існеє 2 основні області видимості:
+// задача: реалізувати обмеження, щоб особи які не досягли повноліття не могли купувати алкогольні та тютюнові вироби
 
- 1. глобальна бласть видимості - охоплює весь документ (програму)
+const isAdult = age < 18 ? 'not adult' : 'adult';
 
-
- */
-
-const globalVariable = 10;
-
-function globalFunction() {
-  // тіло
-}
-
-console.log(globalVariable);
-
-//2. локальна область видимості - визначається блоками коду (фігурними дужками)
-
-function testFunction() {
-  const localVariable = 20; // локальна змінна
-
-  if (true) {
-    const innerVariable = 30; // локальна змінна if
-    console.log(innerVariable); // 30
-    console.log(localVariable); // 20
-    console.log(globalVariable); // 10
-  }
-
-  console.log(localVariable); // 20
-  console.log(innerVariable);
-}
-testFunction();
-
-//console.log(localVariable); - помилка
-
-//summary
-/*
-
-Scope - область видимості
-(це всі наші змінні, обʼєкти ітд, якаа нам доступні)
-
-Local Scope ( локальна область видимості) - це всі наші змінні, обʼєкти ітд ВСЕРЕДИНІ нашої функції
-
-Global Scope (глобальна область видимості) -  це всі наші змінні, обʼєкти ітд з поточного коду
-
-*/
+console.log(isAdult);
