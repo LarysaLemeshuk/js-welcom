cc934093d9e65b86f0e0a70d5be9fa901b63b77e
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2024-12-30 17:38:38 +0100

    links

--- index.js
+++ index.js
@@ -1,60 +1,11 @@
-/*
-задача 2
-Зробити калькулятор
-
-користувач послідовно вводить 
-- переше число
-- дрге число
-- один з варіантів - + * /
-
-Для кожної математичної дії зробіть окрему функію
-весь калькулятор - окрема функція, роль якої - спитати користувача два числа
-*/
-
-function calculator() {
-  const operand1 = Number(prompt("Введіть перше число"));
-  const operand2 = Number(prompt("Введіть друге число"));
-  const action = prompt("Введіть потрібну дію: +, -, *. /");
-
-  let result; // тут буде лежать остаточний результат
-
-  switch (action) {
-    case "+": {
-      result = sum(operand1, operand2);
-      break;
-    }
-    case "-": {
-      result = substract(operand1, operand2);
-      break;
-    }
-    case "*": {
-      result = multy(operand1, operand2);
-      break;
-    }
-    case "/": {
-      result = divide(operand1, operand2);
-      break;
-    }
-    default: {
-      console.log("Сталася помилка!");
-    }
-  }
-  console.log(result);
-}
-calculator();
-
 function sum(a, b) {
   return a + b;
 }
 
-function substract(a, b) {
-  return a - b;
-}
+let superFunction = sum; // насправді, sum - це посилання на комірку в RAM
 
-function multy(a, b) {
-  return a * b;
-}
+sum(2, 4);
+console.log(sum(2, 4));
 
-function divide(a, b) {
-  return a / b;
-}
+console.log(superFunction(5, 5));
+
