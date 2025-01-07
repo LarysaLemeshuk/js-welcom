72112890c7e09a0a334b04eebe8ea64065691543
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-01-07 16:10:30 +0100

    Higt order function

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+/*
+
+Higt Order Function (Фунція вищого порядку)-
+функція, яка приймає іншу функцію в якості аргумента
+
+*/
+
+function calculator(num1, num2, functionLink) {// HOF
+  let result = functionLink(num1, num2);
+  return result;
+}
+
+console.log(calculator(5, 5, sum));
+
+//використовуючи калькулятор, піднесіть num1 у степінь num2
+ console.log( calculator(3, 2, function(a, b ){// callback
+    return a ** b;
+}))
+
 function sum(a, b) {
   return a + b;
 }
 
-let superFunction = sum; // насправді, sum - це посилання на комірку в RAM
+function substract(a, b) {
+  return a - b;
+}
 
-sum(2, 4);
-console.log(sum(2, 4));
+function multy(a, b) {
+  return a * b;
+}
 
-console.log(superFunction(5, 5));
-
+function divide(a, b) {
+  return a / b;
+}
+//callback - функція зворотнього виклику, яку ми передали як аргумент при виклику іншої функції
