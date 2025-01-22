b6e17fab92d59e10f09a5e03e6991ff6c13759e3
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-01-22 17:04:27 +0100

    PRACTICE 1

--- index.js
+++ index.js
@@ -1,70 +1,32 @@
-/* РОзробка прогами для керування книжковим магазином
+/*
+Задача 1:
+зробити функцію-конструктр Country, яка приймає в якості аргументів:
+- назва країни
+- популяція (population)
+- площа (area)
+Спроектувати метод густоти населення
+Густота населення = популяція цієї країни поділена на площу країни
 
- Предметна область: В кножковому магазині багато книг на продаж
-
- всі наші книги можна представити у вигляді обʼєкти з однаковою структурою.
-
- Задача: створити контсруктор обʼєктів для представлення книг з наступними властивостями
- Назва книги, автор, рік видання, ціна
- спроектувати методи для отримання і встановлення значень цих властивостей
-
- Задача 2: зробити для всіх книг метод, який повернув ціну книги , але зі знижкою
+Задача 2:
+зробити функцію-конструктор авто, яку має:
+-назву
+-максимальну швидкість
+-поточну швидкість
+спроектувати методи:
+- метод прискорення (accelearte) - приймає в якості аргумента певне прискорення
+прискорення не може !== максимальній швидкості
+- метод сповільнення (deaccelearte) - приймає в якості аргумента певне сповільнення
+ при сповільненні поточна швидкість не може бути менша за нуль
+ - метод упинки (stop)
 */
 
-function Book(title, author, year, price) {
-  this.title = title;
-  this.author = author;
-  this.year = year;
-  this.price = price;
+function Country(name, population, area){
+  this.name = name;
+  this.population = population;
+  this.area = area;
 
-  this.getTitle = function () {
-    //реалізувати перевірку права на доступ до властивості
-    return this.title;
-  };
-  this.getAutor = function () {
-    //реалізувати перевірку права на доступ до властивості
-    return this.author;
-  };
-  this.getYear = function () {
-    //реалізувати перевірку права на доступ до властивості
-    return this.year;
-  };
-  this.getPrice = function () {
-    //реалізувати перевірку права на доступ до властивості
-    return this.price;
-  };
-
-  this.setTitle = function (newTitle) {
-    //ралізувати перевірки на право на перейменування чи ін.
-    this.title = newTitle;
-  };
-  this.setAutor = function (newAutor) {
-    this.author = newAutor;
-  };
-  this.setYear = function (newYear) {
-    this.year = newYear;
-  };
-  this.setPrice = function (newPrice) {
-    this.price = newPrice;
-  };
-
-  this.calculateDiscountedPrice = function (DiscountPercentage) {
-    const discountedPrice =
-      this.price - this.price * (DiscountPercentage / 100);
-    return discountedPrice;
-  };
+  this.getDensity = function(PopulationDensity){
+    return  this.population / this.area;
+  }
 }
-
-const book1 = new Book("Чотири вітри", "Крістін Генна", 2020, 450);
-
-//console.log(book1.getTitle());
-//console.log(book1.getAutor());
-//console.log(book1.getYear());
-//console.log(book1.getPrice());
-
-//book1.setTitle("New Title");
-//book1.setAutor("New Autor");
-//book1.setYear(2025);
-//book1.setPrice(350);
-
-console.log(book1.calculateDiscountedPrice(90));
+const UK = new Country ('UNITED KINDOM', 6733300, 243610);
