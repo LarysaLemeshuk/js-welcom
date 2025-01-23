3e6658990b95a2ea669701d483ef430c87aff33c
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-01-23 19:40:12 +0100

    practice

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-/*Задача:
-у нас є оюʼєкт з різними версіями привітань для користувача
-вивести користувачу привітання залежно від значення яке він ввів.
-
+/*
+Задача: у нас є обʼєкт, ключем в якому є день тижня, а значенням - плани на цей деь
+прийняти від користувача через промпт назве тижня і видати альорт що заплановано у користувача на цей день
 */
 
-const greets = {
-  15: "hi",
-  17: "ya, whats up",
-  19: "Hello",
-  30: "Good morning",
+const obj = {
+  mon: "drive lesson",
+  tue: "dantist",
+  wed: "go to party",
+  thu: "hard work",
+  fri: "chill",
+  sat: "go to park",
 };
-
-const valueFromUser = 19;
-console.log(greets[valueFromUser]);
+const userValue = prompt("введи день тижня :\n Monday = mon, /ntuesday = tue, /n...");
+alert (obj[userValue] ? obj[userValue] : 'Error happened')
