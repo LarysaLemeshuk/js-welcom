975d0c32875a2ed1626f811fd47c768c5dbf4156
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-01-25 13:09:47 +0100

    practise

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-/*
+//array.concat - використовується для обʼєднання двох або більше масивів і новий масив
+// array.concat(array1, arry2, ..... arrayN)
 
-Задача 3:
-написати функцію, яка знаходить середнє арифметичне всіх елементів масиву.
-Середнє арифметичне = сума всіх елементів масиву / кількість елементів масиву
+const array1 = [1, 2, 3, 4];
+const array2 = array1.concat([222, 888, 333], [555, 777]);
+//console.log(array2);
 
-[3, 6, 8, 2, 3, 5, 1] //4
-*/
+// array.fill()- використовується для заповнення всіх елементві масиву якимсь одним значенням
+// array.fill(value, start, [end]); start, end - не обовʼязкові
 
-const numbersArray = [3, 6, 8, 2, 3, 5, 1];
+const array3 = new Array(5);
+array3.fill(1);
 
-function average(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum / array.length;
-}
+// array.includes - використовується для перевірки наявності певного елементу у масиві
+// array.includes(searchValue,[fromindex])
 
-console.log(average(numbersArray));
+const array4 = [2, 4, 1, 5, 8, 13, 12];
+console.log(array4.includes(5));
+
+//array.indexOf - використовуєтьcя для отримання першого входження певного значення у масиві
+// array.indexOf(searchElements,[fromIndex])
+
+//console.log(array4.indexOf(12));
+
+//array.join - використовується для створення рядка обʼєднуючи всі елементи масиву
+//array.join(separator)
+
+const array5 = ["hello", "world", 4, 2, "hi"];
+array5.join();
+console.log(array5.join());
+//
