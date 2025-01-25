0bb0e1116e4c159f2f10b754c4b5881a56a6419d
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-01-25 12:08:43 +0100

    practse task 3 resolve

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
 /*
 
-Задача 2:
-Дано масив [3, 6, 8, 2, 3, 5, 1]
-Написати функцію, яка знаходить найбільше значення серед елементів масиву.
+Задача 3:
+написати функцію, яка знаходить середнє арифметичне всіх елементів масиву.
+Середнє арифметичне = сума всіх елементів масиву / кількість елементів масиву
 
-1. Запамятати найбільше число, яке ми зустріли
-2. порівняти його з наступним елементом масиву.
-Якщо елемент масиву опиниться більшим ніж те число, яке ми запамʼятали, то саме цей елемент масиву стає найбільш запамʼятовуваним
-
-const numMaximum = 5;
-const currentNum = 4;
-
-if(numMaximum > currentNum){
-// ідемо далі
-}else{numMaximum = currentNum
-}
-
+[3, 6, 8, 2, 3, 5, 1] //4
 */
 
 const numbersArray = [3, 6, 8, 2, 3, 5, 1];
 
-function maxArray(array) {
-  let maxNumber = array[0];
-
-  for (let i = 0; i < array.lehgth; i++) {
-    if (array[i] > max) {
-      // якщо поточний простматреваемій елемент масиву більше, ніж максимальне число, яке ми запамʼятали
-      maxNumber = array[i];
-    }
+function average(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
   }
-  return maxNumber
+  return sum / array.length;
 }
 
-console.log(maxArray(numbersArray));//8
+console.log(average(numbersArray));
