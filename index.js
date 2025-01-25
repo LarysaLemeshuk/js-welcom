40d55608a624936d620372d73fa33a100fb212f4
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-01-25 11:49:45 +0100

    Practise task 2 resolve

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 /*
 
-Задача 1:
-Дано масив [3, 6, 8, 2, 3, 5, 1].
-Напишіть функцію, яка приймає масив в якості аргументу і повертає суму всіх елементів масиву
+Задача 2:
+Дано масив [3, 6, 8, 2, 3, 5, 1]
+Написати функцію, яка знаходить найбільше значення серед елементів масиву.
+
+1. Запамятати найбільше число, яке ми зустріли
+2. порівняти його з наступним елементом масиву.
+Якщо елемент масиву опиниться більшим ніж те число, яке ми запамʼятали, то саме цей елемент масиву стає найбільш запамʼятовуваним
+
+const numMaximum = 5;
+const currentNum = 4;
+
+if(numMaximum > currentNum){
+// ідемо далі
+}else{numMaximum = currentNum
+}
 
 */
 
-const numArr = [3, 6, 8, 2, 3, 5, 1];
+const numbersArray = [3, 6, 8, 2, 3, 5, 1];
 
-function sumOfElementsArr(arr){
-    let res = 0; // Створення змінної, де ми накопичуємо суми 
+function maxArray(array) {
+  let maxNumber = array[0];
 
-    for(let i = 0; i < arr.length; i++){// 2 крок: циклічне накопичення суми
-        res += arr[i];// res = res + arr[i]
+  for (let i = 0; i < array.lehgth; i++) {
+    if (array[i] > max) {
+      // якщо поточний простматреваемій елемент масиву більше, ніж максимальне число, яке ми запамʼятали
+      maxNumber = array[i];
     }
-    return res;// 3 крок: повернення результату
+  }
+  return maxNumber
 }
-console.log(sumOfElementsArr(numArr)); //28
 
+console.log(maxArray(numbersArray));//8
