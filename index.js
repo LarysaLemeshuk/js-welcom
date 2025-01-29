ab6ed603bd62c1fae68b5f8f9ac63564319c1fec
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-01-29 20:19:08 +0100

    task 1 resolve

--- index.js
+++ index.js
@@ -1,41 +1,28 @@
-function saySomething(howToSay, whatToSay) {
-  howToSay(whatToSay);
+const users =[{
+    name: 'John',
+    lastName: 'Doe',
+    age: 19,
+    email: 'john.doe@gmail.com'
+}, {
+    name: 'Jone',
+    lastName: 'Doe',
+    age: 22,
+    email: 'jone.doe@gmail.com'
+}, {
+    name: 'Jackson',
+    lastName: 'Doe',
+    age: 55,
+    email: 'jacksone.doe@gmail.com'
+}]
+
+// Задача 1: збільшити вік на 1
+/*
+function plusOneYear (item){
+    item.age +=1;
 }
+array.forEach(plusOneYear)
+ */
 
-//saySomething(alert, 'Hello user')
-//saySomething(console.log, "hi");
-
-// як називаються функції, які приймають інші функції в якості аргумента
-//HOF -High Order Function
-
-//як називається функція яку ми передали як аргумент
-//callback - функція зворотнього виклику
-
-// array.ForEach - використовується для перебору (ітерації) кожного елемента масиву і виконання певної дії для кожного елемента
-// array.ForEach(callback)
-
-// Задача6 у нас є масив чисел
-// Кожне значення цього масиву привести в квадрат
-
-const array = [1, 2, 3, 4, 5];
-
-function square(a) {
-  console.log(a * a);
-}
-
-array.forEach((item) => {
-  console.log(item * item);
-});
-
-
-//array.map - працює як forEach, тільки map буде повертати новий масив
-function square(a){
-   return a*a 
-}
-
- const newArray = array.map((item) =>{
-    return item * item
- });
-
- // коли ми використовуємо map, в такому випадку, значення, яке ви повертаєте (return) з колбеку - воно і буде входити в результуючий масив
- 
+users.forEach((item) => item.age +=1);
+console.log(users);
+  
