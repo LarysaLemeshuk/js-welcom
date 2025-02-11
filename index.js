fc56ca4bd177819625e413f4405c91d42310fca9
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-02-11 17:06:13 +0100

    Add pop mathod + task

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ function MyArray(){
   this.length = 0;
 
   this.push = function (value){
-    // значення додати у кінець масиву
-    this[this.length] = value;
-    this.length ++;
+   for(let i =0; i < arguments.length; i++){
+     this[this.length] = arguments[i];
+     this.length ++;
+   }
     return this.length;
+  }
+  this.pop = function(){
+    if(this.length > 0){
+// зберегти останній елемент
+const lastItem = this[this.length - 1];
+// видалити останній елемент з масиву
+delete this[ this.length -1];
+// зменшити довжину масиву на 1
+this.length --;
+// повернути останній елемент
+return lastItem;
+    } else{
+      return undefined;
+    }
+  }
+  this.forEach = function(callback){
+    
   }
 }
 
@@ -14,7 +32,7 @@ arr.push(1);
 arr.push(2);
 
 //Перепишіть push таким чином, щоб він міг приймати та додавати до масиву будь-яку кількість переданих елементів
-// arr.push(3, 2, 5, 10, 12, 33);
+arr.push(3, 2, 5, 10, 12, 33);
 
 
 
