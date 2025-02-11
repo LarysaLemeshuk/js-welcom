3a1cd598fc674e5bb1941eae5e12bb66591e4ffc
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-02-11 18:27:02 +0100

    Three types of function

--- index.js
+++ index.js
@@ -1,96 +1,37 @@
-function MyArray() {
-  this.length = 0;
+'use strict';
+//strict mode
+
+console.log(this); // => window
+
+// this  у глобальній області видимості посилається на обʼєкт Window
+
+function test() {
+  // Function Declaration
+  console.log(this); // => undefind
 }
 
-function MyArrayPrototype() {
-  (this.push = function (value) {
-    for (let i = 0; i < arguments.length; i++) {
-      this[this.length] = arguments[i];
-      this.length++;
-    }
-    return this.length;
-  }),
-    (this.pop = function () {
-      if (this.length > 0) {
-        // зберегти останній елемент
-        const lastItem = this[this.length - 1];
-        // видалити останній елемент з масиву
-        delete this[this.length - 1];
-        // зменшити довжину масиву на 1
-        this.length--;
-        // повернути останній елемент
-        return lastItem;
-      } else {
-        return undefined;
-      }
-    }),
-    (this.forEach = function (callback) {
-      for (let i = 0; i < this.length; i++) {
-        callback(this[i], i, this);
-      }
-    });
-}
+test();
 
-MyArray.prototype = myArrayPrototype; // рпототипне посилання
+//Function Expression
+const test2 = function () {
+  console.log(this); // undefined
+};
 
-const arr = new MyArray();
-arr.push(1);
-arr.push(2);
-arr.push(3, 2, 5);
+test2();
 
-// відконсольлогувати квадрати кожного числа в масиві arr
+console.log(this);// window
 
-arr.forEach((item) => {
-  console.log(otem ** 2);
-});
-
-// Яка різниця між __proto__  та .prototype
-
-// __proto__ працює тільки тоді, коли ми літерально створили обʼєкт
-// .prototype - коли обʼєкти створюються за допомогою конструктора
+// весь код, оточений фігурними дужками - контекст виконання
 
 /*
-Задача 1
-створити функцію-конструктор для сходів.(ladder)
-обʼєкт має властивість:
-currentStair - сходинка, на якій ми зараз знаходимосью Початково = 0
-
-Має методи:
-up()- піднімає на сходинку вище
-down()- опускає на сходинку нижче
-showStair()- показує, на якій сходинці ми зараз знаходимось
+Strict mode запустив такий механізм, що у Function Declaration ta Function Expression зʼявляється свій власний контекст виконання
+контекстом виконання функції стає сама функція, а не глобальний обʼєкт window
 */
 
-function Ladder() {
-  this.currentStair = 0;
+// Arrow Function
+const test3 =() => {
+  console.log(this);//this  вказує на глобальний обʼєкт Wndow
+  // Стрілочна функція не має свого власного контексту виконання
 }
 
-function LadderPrototype() {
-  this.up = function () {
-    this.currentStair++;
-    return this;
-  };
-
-  this.down = function () {
-    this.currentStair--;
-    return this;
-  };
-
-  this.showStair = function () {
-    return this.currentStair;
-
-  };
-}
-
-const ladder = new Ladder();
-
-console.log(ladder.up().up().down().up().showStair());
-
-/*
-Задача 2
-Перепишіть методи таким чиномБ щоб можливо було використання чейнінгу,
-
-const ladder =  newLadder
-
-ladder.up().up().down().up().showStair();
-*/
+test3();
