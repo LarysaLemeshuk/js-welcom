f84efa29609727ddc1f792c1d67eefb23226c645
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-02-13 18:28:31 +0100

    Practise tasck resolve

--- index.js
+++ index.js
@@ -22,11 +22,34 @@ console.log(sum(1, 2, 3, 5, 10, 25)); //3
 
 //Задача: написати стрілочну функцію, яка сумує будь-яку кількість чисел
 
-const arrowSum = (...restArrayOfNumbers) => {
+/*const arrowSum = (...restArrayOfNumbers) => {
   let sum = 0;
 
   for (let i = 0; i < restArrayOfNumbers.length; i++) {
     sum += restArrayOfNumbers[i];
   }
   return sum;
-};
+};*/
+
+/*
+Задача
+Напишіть варіант функції arrowSum з використанням метода reduse
+ПРи виклику передайте йому стрілковий колбек
+
+*/
+
+/*const arrowSum = (...restArray) => {
+  const sum = restArray.reduce(accumulator, currentValue) => {
+return accumulator + currentValue;
+  }
+  return sum;
+}*/
+
+/*const arrowSum = (...restArray) => {
+  const sum = restArray.reduce(accumulator, currentValue) => {
+return accumulator + currentValue;
+  }
+  return sum;
+}*/
+
+const arrowSum = (...restArray) => restArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
