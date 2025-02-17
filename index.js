d4bc8f6123032290342c6ee8dffcc73b695ab782
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-02-17 20:33:26 +0100

    practise 1

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-/*
+/* 
 
-Написати функцію, яка приймає рядок і робить кожну перщу літеру кожного слова з великої літери
+  Задача 1
+Написати функцію checkSpam, яка повертає true, якщо переданий рядок містить слова 'ххх' або 'viagra'
+Якщо заборонених слів у рядку немає - повертається false
+
+checkSpam ( 'buy ViAgRa now'); // true 
+checkSpam( 'free xxxxxxx'); // true 
+checkSpam( 'innocent rabbit'); // false
 
 */
 
-function capitalizeWords(str) {
-  // Розбиваємо рядок на окремі слова
-  let words = str.split(' ');
+function checkSpam(str) {  // variant 2
 
-  // Проходимо по кожному слову і робимо першу літеру великою
-  for(let i = 0; i < words.length; i++) {
-   words [i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  const spamArray = ['viagra', 'xxx', 'drugs'];
+
+  for (let i = 0; i < spamArray.length; i++) {
+    if (str.toLowerCase().includes(spamArray[i])) {
+      return true;
+    }
   }
-  // Повертаємо зʼєднаний рядок зі словами, розділеними пробілами
-  return words.join(' ');
-  }
-  console. log(capitalizeWords( 'word flower third'));
+  return false;
+}
+
+console.log(checkSpam('buy ViAgRa now'));
+console.log(checkSpam('free xxxxxxx'));
+console.log(checkSpam('innocent rabbit'));
+
+/* 
+Задача 2
+
+Написати функцію, яка перевіряє, чи є переданий їй рядок - паліндромом. не зважаючи на регістр 
+Паліндром - це коли рядок з обох сторін читається однаково
+
+Anna - паліндром
+Мата - не паліндром
+Namman - паліндром
+*/
