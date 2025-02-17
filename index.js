796345fa8fd8a75712e616be5aaf90ef09995e5d
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-02-17 17:36:39 +0100

    Double Loop

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-'use strict';
+// вкладені цикли
 
-// rest operator - оператор залишкових аргументів
-// rest - залишок
-// .., .., .., .., -->> []
+/*
 
-// spread operator
-// spread - розпакувати
-// [] --->> .., .., ..,
+Вивести на консоль таблицю множення від 1 до 5
 
-const numbers = [1, 2, 3, 4, 5];
+1x1 = 1
+1x2 = 2
+...1x5 = 5
 
-function sum(a, b, ...restArray){
-  //console.log(restArray);
-  return a + b;
+2x1 = 2
+2x2 = 4
+
+....
+5x1 = 5
+5x2 = 10
+5x3
+5x4
+5x5
+
+*/
+/*
+for(let i = 1; i <=5; i ++){
+  for(let j=1; j <=5; j++){
+    console.log(`${i} x ${j} = ${i*j}`);
+  }
 }
-//console.log(sum(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]));
-console.log(sum(...numbers))// spread operator
-//розпакувати всі значенння масиву в виклик функції sum
+  */
 
+const numbers =[[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
-// приклади використання spread operator
-const numbersMath = [2, 3, 1, 5, 4];
+// Задача: знайти суму елементів масиву
 
-Math min (...numbersMath);
-
-// Задача
-
-const aray1 = [1, 2, 3, 4, 5];
-const array2 =[10, 11, 12, 13, 14, 15];
-
-// Потрібно зклеїти ці 2 масиви
-// Не можна використовувати метод concat
-// Використовуйте spread
-
-const resultArray = [ ...aray1, ...array2];
+let sum = 0;
+for(let i = 0; i < numbers.length; i++){
+  for(let j = 0; j < numbers[i].length; j++){
+    sum += numbers[i][j]
+  }
+}
