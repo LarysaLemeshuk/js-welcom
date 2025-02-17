ec6ab187ea93ff7dad402efbb2d671c12d6a9d0a
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-02-17 16:55:37 +0100

    spread operator

--- index.js
+++ index.js
@@ -1,55 +1,25 @@
 'use strict';
 
-// Задача:
-/*
-у фенкції sum знайти суму двох елементів, а усі інші аргументи, які передали у функцію ззовні, при виклику - покласти у масив
-*/
-
 // rest operator - оператор залишкових аргументів
 // rest - залишок
 // .., .., .., .., -->> []
 
-function sum(a, b, ...arrayOfRestArguments) {
-  console.log(arrayOfRestArguments);
+// spread operator
+// spread - розпакувати
+// [] --->> .., .., ..,
+
+const numbers = [1, 2, 3, 4, 5];
+
+function sum(a, b, ...restArray){
+  //console.log(restArray);
   return a + b;
 }
+//console.log(sum(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]));
+console.log(sum(...numbers))// spread operator
+//розпакувати всі значенння масиву в виклик функції sum
 
-console.log(sum(1, 2, 3, 5, 10, 25)); //3
 
-//function f(arg1, ...rest, arg2){ // помилка
+// приклади використання spread operator
+const numbersMath = [2, 3, 1, 5, 4];
 
-//}
-
-//Задача: написати стрілочну функцію, яка сумує будь-яку кількість чисел
-
-/*const arrowSum = (...restArrayOfNumbers) => {
-  let sum = 0;
-
-  for (let i = 0; i < restArrayOfNumbers.length; i++) {
-    sum += restArrayOfNumbers[i];
-  }
-  return sum;
-};*/
-
-/*
-Задача
-Напишіть варіант функції arrowSum з використанням метода reduse
-ПРи виклику передайте йому стрілковий колбек
-
-*/
-
-/*const arrowSum = (...restArray) => {
-  const sum = restArray.reduce(accumulator, currentValue) => {
-return accumulator + currentValue;
-  }
-  return sum;
-}*/
-
-/*const arrowSum = (...restArray) => {
-  const sum = restArray.reduce(accumulator, currentValue) => {
-return accumulator + currentValue;
-  }
-  return sum;
-}*/
-
-const arrowSum = (...restArray) => restArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+Math min (...numbersMath);
