2435fcdddca783cb08f035d626436bebb4e7b032
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-02-18 21:20:42 +0100

    Date object

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-/* 
-Задача 2
+const date = new Date(); // за замовчуванням буде створена поточна системна дата та час
+const date1 = new Date('12-31-2023'); // 31.12.2024
 
-Написати функцію, яка перевіряє, чи є переданий їй рядок - паліндромом. не зважаючи на регістр 
-Паліндром - це коли рядок з обох сторін читається однаково
+console.log(date1.getDate()); // 31; getDate використовується для отримання місяця з дати
+console.log(date1.getDay()); // використовується для отримання дня тижня з дати
+console.log(date1.getFullYear()); // використовується для отримання року з дати
+console.log(date.getHours()); // використовується для отримання години з дати
+console.log(date1.getTime()); // використовується для отримання  кількості мілісекунд, які пройшли з 1 січня 1970 року 00:00:00
 
-Anna - паліндром
-Мата - не паліндром
-Namman - паліндром
-*/
+// Задача: знайи суму першого 1000000 чисел
 
-function checkPalindrom(str) {
-  /*
-  +1. Приймаємо строку
-  +2. Приводимо строку до нижнього регістру
-  +3. Перевернути строку
-  +4. Порівнюємо перевернуту строку з пункту 3 з оригінальною строкою
-  */
+let sum = 0;// створюємо змінну
 
-  // Реалізація 1 і 2 пункту
-  const originalStr = str.toLowerCase();
+const time1 = new Date();// відрізок часу ДО початку операції
 
-  //Реалізація 3 пункту
-  const reversedStr = originalStr.split('').reverse().join('');
-
-  //Реалізація 4 пункту
-  return originalstr === reversedStr;
+for (let i = 0; i <= 1000000; i++) {// накопичуємо суму
+  sum += i;
 }
 
-console.log(checkPalindrom('Papa')); // false
-console.log(checkPalindrom('Anna')); // true
+const time2 = new Date();// відрізок часу після операції
+
+console.log(time2.getTime() - time1.getTime());
+
+
+
+// приклад з консоль. таймами
+console.time('operation')// включили таймер
+let sum2 = 0;
+for (let i = 0; i <= 1000000; i++) {// накопичуємо суму
+  sum += i;
+}
+//......
+
+console.timeEnd('operation'); 
