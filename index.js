f78222e425409bf73ec93348a06dc4be07ccdbc0
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-03-02 17:15:54 +0100

    Extends example v2

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
-class Animal {
-  constructor(nicname, color) {
-    this.nicname = nicname;
-    this.color = color;
+class User {
+  constructor(name, surname, age) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
   }
-  eat() {
-    return `${this.nicname} is eating`;
-  }
-}
-
-//class Dog extends Animal клас Собаки розирює клас Тварини ( наслідує цей клас)
-class Dog extends Animal {
-  constructor(nicname, color) {
-   super(nicname, color);
-  }
-  gav() {
-    return `${this.nicname} gav-gav-gav`;
+  getFullName() {
+    return `${this.name} ${this.surname}`;
   }
 }
 
-const dog = new Dog('Tuzik', 'grey');
+class Moderator extends User {
+  constructor(name, surname, age) {
+    super(name, surname, age);
+  }
+
+  getFullName() {
+    return `${this.name} ${this.surname}---> ${this.age}`;
+  }
+  greatePost(text) {
+    console.log('Post successfully created');
+  }
+  deletPost(id) {
+    console.log('Post successfully deleted ');
+  }
+}
+
+class Admin extends Moderator {
+  constructor(name, surname, age, uniqePrefix) {
+    super(name, surname, age);
+    this.uniqePrefix = uniqePrefix;
+  }
+
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+  makeModerator(userId) {
+    console.log('Moderator successfully delited');
+  }
+}
+
+class Support extends Admin{
+  constructor(name, uniqePrefix){
+    super(name, null, null, uniqePrefix);
+  }
+  getFullName() {
+    return `${this.name}---> ${this.age}`;
+  }
+}
+
+const user = new User('John', 'Doe', 32);
+
+const moderator = new Moderator('Alex', 'Krane', 45);
+
+const admin = new Admin('Jane', 'Doe', 50, 'Head of Sales');
+
+const support = new Support('William', 'Head of Support');
