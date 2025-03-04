1299bf94de4edb82eb6a002c4fa0d8fe92077151
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-03-04 20:29:31 +0100

    OOP welcome

--- index.js
+++ index.js
@@ -30,7 +30,10 @@
 
 // Спадкування - здатність до копіювання, можливість описати новий клас на основі вже існуючого
 
-class Figure {
+// Поліморфізм - дозволяє використовувати спільний інтерфейс (методи базовоо класу через екемпляри похідних класів)
+// Суть поліморфізму полягає в тому, що ми можемо один і той самий код використовувати для обʼєктів різних класів
+
+class Figure {// абстрактний клас
   constructor(sideQuantity) {
     this.sideQuantity = sideQuantity;
   }
@@ -45,7 +48,7 @@ class Figure {
     }
     this.sideQuantity = newValue;
   }
-  getArea() {}
+  getArea() {} // абстрактні методи
 }
 
 class Triangle extends Figure {
@@ -88,7 +91,7 @@ class Triangle extends Figure {
     this._angle = newValue;
   }
 
-  getArea() {
+  getArea() { // віртуальний метод
     return this.a * this.b * Math.sin(this.angle);
   }
 }
@@ -110,9 +113,38 @@ class Square extends Figure {
     this._a = newValue;
   }
 
-  getArea() {
+  getArea() { // віртуальний метод
     return this.a * this.a; // .this.a ** 2
   }
 }
 
 const square = new Square(4);
+const triangle = new Triangle(5, 6, 45)
+
+
+// Задача: написати окрему функцію, яка буде приймати обєкт якоїсь фігури і буде викликати у цієї фігури метод getArea
+
+function getFigureArea(figure){
+  if(figure instanceof Figure){
+    return figure.getArea();
+  }
+  throw new TypeError (' Параметр не є фігурою')
+}
+
+/*
+
+Переваги
+
+1. Візуально код стає простішим
+2. Менше однакового коду
+3. Складні програми пишуться простіше
+4. Збільшується швидкість написання ( достатньо швидко можемо прийти до мінімально працюючого прототипу)
+
+Недоліки
+
+1. ВАжко зрозуміти та почати працювати
+2. Потребує більше памʼяті
+3. Іноді продуктивність коду може бути досить низька
+(із сучасними потужностями проіесорів недолік № 3 нівелюється)
+
+*/
