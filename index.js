758af5d71a556ba88e56dcf60f334bf7078761eb
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-03-11 21:51:56 +0100

    Factory mathod

--- index.js
+++ index.js
@@ -1,150 +1,72 @@
-// ООП
-
-/* Загальні ідеї ООП
-
-1. ООП збудоване навколо оббʼєктів
-2. Поділ потрібний для того, щоб можна було створювати, обслуговувати та перетворювати якісь частини програми, не впиваючи прицьому на інші.
-3. Сенс обʼєкти у тому, що він має якісь методи, тобто він вміє щось робити, при цьому, інші обʼєкти  не знають як він це робитью
-4. Обʼєкт має поняття атрибуту і методу
-Атрибут - будь-які дані які зберігаються в середині обʼєкта
-Методи - будь-які дії, які можна здійснювати над обʼєктом , атрибутами обʼєкта.
-Атрибути зазвичай виражаються змінними, а методм виражаються функціями.
-5. Оюʼєкти можна створювати за шаблоном - такі шаблони називають класами.
-
-
-*/
-
-// Геттери та сеттери - аксессори (від англ. access - доступ)
-
-// Абстракція - коли ми зосереджуємось тільки на суттєвих для завданнях деталях (коли описуємо обʼєкт) і ігноруємо все інше.
-// Чим менше характеристик у обʼєкта, тим краща абстракція, ае ключові характеристики не можна прибирати!
-
-// Щоб працювати з абстракціями , використовують інтерфейси
-// Інтерфейс - спосіб взаємодії з обʼєктом, який визначає, які операції можна здійснити з обʼєктом, які дані можна отримати або змінити
-
-// Інкапсуляція - процес обʼєднання даних та методів в одному обʼєкті і приховування деталей реалізації від користувача
-
-// В чому разниця між інкапсуляцією та абстракцією?
-// Інкапсуляція зосереджена на організації та приховувані деталей реалізації обʼєкта
-// Абстракція спрощує складність системи шляхом виділення ключових аспектів та приховуванні незначних деталей
-
-// Спадкування - здатність до копіювання, можливість описати новий клас на основі вже існуючого
-
-// Поліморфізм - дозволяє використовувати спільний інтерфейс (методи базовоо класу через екемпляри похідних класів)
-// Суть поліморфізму полягає в тому, що ми можемо один і той самий код використовувати для обʼєктів різних класів
-
-class Figure {// абстрактний клас
-  constructor(sideQuantity) {
-    this.sideQuantity = sideQuantity;
-  }
-
-  get sideQuantity() {
-    return this.sideQuantity;
-  }
-
-  set sideQuantity(newValue) {
-    if (newValue < 0) {
-      throw new RangeError('Side cannot be less then 0');
-    }
-    this.sideQuantity = newValue;
-  }
-  getArea() {} // абстрактні методи
-}
-
-class Triangle extends Figure {
-  constructor(a, b, angle) {
-    super(3);
-    this.a = a;
-    this.b = b;
-    this.angle = angle;
-  }
-  get a() {
-    return this._a;
-  }
-
-  set a(newValue) {
-    if (newValue < 0) {
-      throw new RangeError('Side cannot be less then 0');
-    }
-
-    this._a = newValue;
-  }
-
-  get b() {
-    return this._b;
-  }
-  set b(newValue) {
-    if (newValue < 0) {
-      throw new RangeError('Side cannot be less than 0');
-    }
-    this._b = newValue;
-  }
-
-  get angle() {
-    return this._angle;
-  }
-
-  set angle(newValue) {
-    if (newValue < 0) {
-      throw new RangeError('Angle cannot be less than 0');
-    }
-    this._angle = newValue;
-  }
-
-  getArea() { // віртуальний метод
-    return this.a * this.b * Math.sin(this.angle);
-  }
-}
-
-class Square extends Figure {
-  constructor(a) {
-    super(4);
-    this.a = a;
-  }
-
-  get a() {
-    return this._a;
-  }
-
-  set a(newValue) {
-    if (newValue < 0) {
-      throw new RangeError('Side cannot be less than 0');
-    }
-    this._a = newValue;
-  }
-
-  getArea() { // віртуальний метод
-    return this.a * this.a; // .this.a ** 2
-  }
-}
-
-const square = new Square(4);
-const triangle = new Triangle(5, 6, 45)
-
-
-// Задача: написати окрему функцію, яка буде приймати обєкт якоїсь фігури і буде викликати у цієї фігури метод getArea
-
-function getFigureArea(figure){
-  if(figure instanceof Figure){
-    return figure.getArea();
-  }
-  throw new TypeError (' Параметр не є фігурою')
-}
-
 /*
 
-Переваги
+Фабричний метод - спосіб створювати обʼєкти, який дозволяє нам не вказувати конкретний клас обʼкта.
+Використовуємо спеціальну 'фабрику для' обʼєктів.
+Фабрика знає, який саме обʼєкт створити і повертає його нам
 
-1. Візуально код стає простішим
-2. Менше однакового коду
-3. Складні програми пишуться простіше
-4. Збільшується швидкість написання ( достатньо швидко можемо прийти до мінімально працюючого прототипу)
+new Student();// не робимо!
 
-Недоліки
-
-1. ВАжко зрозуміти та почати працювати
-2. Потребує більше памʼяті
-3. Іноді продуктивність коду може бути досить низька
-(із сучасними потужностями проіесорів недолік № 3 нівелюється)
+Зверьаємось до фабрики, фабриці повідомляємо, що нам потрібно, фабрика уже буде робити нам екземпляри потрібного класу (new Student)
 
 */
+
+// Абстракний клас або інтерфейс
+class Animal {
+  constructor(nickname) {
+    this.nickname = nickname;
+  }
+
+  makeSound() {
+    // абстракний метод
+  }
+}
+
+class Dog extends Animal {
+  constructor(nickname) {
+    super(nickname);
+  }
+  makeSound() {
+    // віртуальний метод
+    console.log('гав-гав');
+  }
+}
+
+// Конкретний клас для кота
+class Cat extends Animal {
+  constructor(nickname) {
+    super(nickname);
+  }
+  makeSound() {
+    // віртуальний метод
+    console.log('Мяу-мяу');
+  }
+}
+
+// Фабричний клас, який створює обʼєкти
+
+class AnimalFactory {
+   static createAnimal(type, nickname) {
+    switch (type) {
+      case 'dog':
+        return new Dog(nickname);
+      case 'cat':
+        return new Cat(nickname);
+      default:
+        throw new Error(`Невідомий тип тварини:${type}`);
+    }
+  }
+}
+
+// Використання фабрики для створення обʼєктів
+
+const factory = new AnimalFactory();
+
+// Створити обʼєкт собаки
+
+const dog = AnimalFactory.createAnimal('dog', 'tuzik');
+dog.makeSound(); // Гав-гав
+
+// створити обʼєкт кота
+
+const cat = AnimalFactory.createAnimal('cat', 'Murzik');
+cat.makeSound(); // Мяу-мяу
