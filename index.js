9fac5ee5788be438226926464339c98dbe5d5621
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-03-12 11:04:17 +0100

    Add method to MyArray

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-/*
+class MyArray {
+  constructor() {
+    this.length = 0;
+  }
+  push() {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      this.length++;
+    }
+    return this.length;
+  }
 
-Symbol - представляє собою унікальний індетифікатор.
-Кожен створений символ має свій унікальний індетифікатор, який ніде і ніколи е буде повторюватись
+  pop() {
+    if (this.length > 0) {
+      // зберегти останній елемент
+      const lastItem = this[this.length - 1];
+      // видалити останній елемент з масиву
+      delete this[this.length - 1];
+      // зменшити довжину масиву на 1
+      this.length--;
+      // повернути останній елемент
+      return lastItem;
+    } else {
+      return undefined;
+    }
+  }
 
-Особливість символів - вони завжди унікальні
-Кожен створений символ, матиме унікальний індетифікатор, який ніде і ніколи не буде повторюватись
+  forEach(callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  }
 
-Гарантія унікальності - єдине, для чого Symbol  і існує
+  map(callback) {
+    // 1. Просто створили новий масив - він буде результуючим масивом
+    const resultArray = new MyArray();
 
-*/
+    // 2. ПРойтись по поточному масиву від початку до кінця
+    for (let i = 0; i < this.length; i++) {
+      // Запихуємо у результуючий масив значення, ке нам буде повертати (return) колбек
+      resultArray.push(callback(this[i], i, this));
+    }
 
-const mySymbol = Symbol(''); // без new!
+    // 3. ПОвернути як результат роботи методу map - результуючий масив
+    return resultArray;
+  }
+}
 
-// const symb2 = Symbol('My second symbol');// label for human
+const arr = new MyArray();
 
-const obj = {
-  //  У обʼєктах ключами можуть бути рядки або символи
-  test: 1,
-  [mySymbol]: 123,
-};
+arr.push(3, 2, 1, 2, 3);
 
-console.log(obj[mySymbol]);
+// Задача: створити новий масив, який буде містии квадрати чисел з масиву arr
+
+const doubleNumbers = arr.map(item => item ** 2);
